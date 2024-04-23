9b7ed1a4a134d150ece64f0e93542195be7f0b43
Hoang Phat <latruonghoangphat@gmail.com>
2024-04-24 02:13:12 +0700

    delete from dev branch

--- ShopFA/src/main/java/com/ITe/ShopFA/controller/OTPController.java
+++
@@ -1,4 +0,0 @@
-package com.ITe.ShopFA.controller;
-
-public class OTPController {
-}
--- ShopFA/src/main/java/com/ITe/ShopFA/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ITe.ShopFA.repository;
-
-import com.ITe.ShopFA.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
